c3e59185d05df609e293901efedc83b907db3295
Tavitu97 <43606487+Tavitu97@users.noreply.github.com>
2022-03-25 13:13:36 +0200

    Consumer PHP index and css
    Index page and header page

---
+++ Consumer/Index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="fonts/fonts.css">
+	<link rel="stylesheet" href="css/style.css">
+	<title>Country overview</title>
+</head>
+<body>
+
+	<?php include 'header.php';?>
+
+	<div class="banner">
+	</div>
+
+	<h1 class="announce"></h1>
+
+	<script type="text/javascript" src="vendor/fusioncharts/js/fusioncharts.js"></script>
+	<script type="text/javascript" src="vendor/fusioncharts/js/fusioncharts.maps.js"></script>
+	<script type="text/javascript" src="vendor/fusioncharts/js/themes/fusioncharts.theme.fusion.js"></script>
+
+
+
+</body>
+</html>
---
+++ Consumer/header.php
@@ -0,0 +1,16 @@
+<header class="header">
+    <div class="container">
+        <div class="header-wrapper  clearfix">
+            <div class="header_logo">
+                <h1 class="logo--smtxt"><span class="logo--lgtxt">Countries </span>Overview</h1>
+            </div>
+            <div class="header_navigator">
+                <ul class="header_menu">
+                    <li class="header_menu_li"><a href="index.php" class="menu_li_links">Index</a></li>
+                    <li class="header_menu_li"><a href="php/jsonRoutes.php" class="menu_li_links">Json routes</a></li>
+                    <li class="header_menu_li"><a href="php/xmlRoutes.php" class="menu_li_links">Xml routes</a></li>
+                </ul>
+            </div>
+        </div>  
+    </div>
+</header>
